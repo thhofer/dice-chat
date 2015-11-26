34e21ba01775e08189d1bdceefd729218b4f0421
thhofer <thomas.geek.hofer@gmail.com>
2015-11-26 22:21:14 +0100

    - fix test

--- test/model/MessageConvertersTests.scala
+++ test/model/MessageConvertersTests.scala
@@ -1,14 +1,15 @@
 package model
 
 import org.specs2.mutable.Specification
-import model.MessageParser._
 
 /**
   * Missing javadoc!
   */
 class MessageConvertersTests extends Specification {
+  val messageParser = new MessageParser("TestUser")
+
   "The 'Hello world' string" should {
-    val parseResult = parse(message, "Hello world")
+    val parseResult = messageParser.parse(messageParser.message, "Hello world")
     "be parsed successfully" in {
       parseResult.isEmpty mustEqual false
     }
@@ -16,12 +17,12 @@ class MessageConvertersTests extends Specification {
       parseResult.get must haveClass[SimpleMessage]
     }
     "with the adequate message" in {
-      parseResult.get mustEqual SimpleMessage("Hello world")
+      parseResult.get mustEqual SimpleMessage("TestUser", "Hello world")
     }
   }
 
   "The '/me sighs' string" should {
-    val parseResult = parse(MessageParser.action, "/me sighs")
+    val parseResult = messageParser.parse(messageParser.action, "/me sighs")
     "be parsed successfully [with action]" in {
       parseResult.isEmpty mustEqual false
     }
@@ -29,12 +30,12 @@ class MessageConvertersTests extends Specification {
       parseResult.get must haveClass[ActionMessage]
     }
     "with the adequate values" in {
-      parseResult.get mustEqual ActionMessage("sighs")
+      parseResult.get mustEqual ActionMessage("TestUser", "sighs")
     }
   }
 
   "The '/roll 3d6' string" should {
-    val parseResult = parse(message, "/roll 3d6")
+    val parseResult = messageParser.parse(messageParser.message, "/roll 3d6")
     "be parsed successfully" in {
       parseResult.isEmpty mustEqual false
     }
@@ -42,12 +43,12 @@ class MessageConvertersTests extends Specification {
       parseResult.get must haveClass[RollMessage]
     }
     "with the adequate values" in {
-      parseResult.get mustEqual RollMessage(3, 6)
+      parseResult.get mustEqual RollMessage("TestUser", 3, 6)
     }
   }
 
   "The '/roll 3d6' string" should {
-    val parseResult = parse(roll, "/roll 3d6")
+    val parseResult = messageParser.parse(messageParser.roll, "/roll 3d6")
     "be parsed successfully [with roll]" in {
       parseResult.isEmpty mustEqual false
     }
@@ -55,12 +56,12 @@ class MessageConvertersTests extends Specification {
       parseResult.get must haveClass[RollMessage]
     }
     "with the adequate values" in {
-      parseResult.get mustEqual RollMessage(3, 6)
+      parseResult.get mustEqual RollMessage("TestUser", 3, 6)
     }
   }
 
   "The 'd6' string" should {
-    val parseResult = parse(diceSides, "d6")
+    val parseResult = messageParser.parse(messageParser.diceSides, "d6")
     "be parsed successfully [with diceSides]" in {
       parseResult.isEmpty mustEqual false
     }
